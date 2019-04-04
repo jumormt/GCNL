c648f5696f3dd3c8f40e8a579cffde15cc32fa95
jumormt <jumormt@163.com>
2019-04-04 16:38:26 +0800

    [update] graphClassify 测试

--- graphClassify.py
+++ graphClassify.py
@@ -135,7 +135,8 @@ def getXY(graphs):
     logger.info(sorted(Counter(y_resampled).items()))
     # print(sorted(Counter(y_resampled).items()))
 
-    # 预处理 归一化正则化
+    # 预处理 (X-mean)/std  计算时对每个属性/每列分别进行。
+    # 将数据按期属性（按列进行）减去其均值，并处以其方差。得到的结果是，对于每个属性/每列来说所有数据都聚集在0附近，方差为1。
     scaler = preprocessing.StandardScaler().fit(X_resampled)
     X_train_transformed = scaler.transform(X_resampled)
 
